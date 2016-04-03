d1e1478c71b033adb144f04ba5c07b3017dcfdd8
Nguyễn Duy Bảo <duy13010@gmail.com>
2016-04-04 04:45:01 +0700

    Cập nhật trang 404

--- catalog/language/vietnamese/error/not_found.php
+++ catalog/language/vietnamese/error/not_found.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title'] = 'The page you requested cannot be found!';
+$_['heading_title'] = 'Rất tiếc không tìm thấy trang';
 
 // Text
-$_['text_error']    = 'The page you requested cannot be found.';
+$_['text_error']    = 'Trang bạn đang tìm không tồn tại hoặc đã bị xóa.';
